48e3aded01b3b974fd665ed2e03660d5ff3453ed
Dan Voell <danvoell@gmail.com>
2023-04-07 21:29:26 -0500

    Add files via upload

---
+++ outline-.125inch-bleed.jsx
@@ -0,0 +1,45 @@
+#target illustrator
+
+(function () {
+    if (app.documents.length === 0) {
+        alert("No document open!");
+        return;
+    }
+
+    if (app.activeDocument.selection.length === 0) {
+        alert("No image selected!");
+        return;
+    }
+
+    var selectedItem = app.activeDocument.selection[0];
+    if (selectedItem.typename !== "PlacedItem") {
+        alert("Selected item is not an image!");
+        return;
+    }
+
+    var imageWidth = selectedItem.width;
+    var imageHeight = selectedItem.height;
+    var imageTopLeft = [selectedItem.position[0], selectedItem.position[1]];
+
+    var strokeWidth = 2;
+    var borderColor = new RGBColor();
+    borderColor.red = 255;
+    borderColor.green = 0;
+    borderColor.blue = 255;
+
+    var border = app.activeDocument.pathItems.rectangle(imageTopLeft[1] + strokeWidth, imageTopLeft[0] - strokeWidth, imageWidth + 2 * strokeWidth, imageHeight + 2 * strokeWidth);
+    border.strokeWidth = strokeWidth;
+    border.filled = false;
+    border.strokeColor = borderColor;
+
+    // Shrink the border by 0.125 inches on each side and center it inside the image
+    var offset = 0.125 * 72; // Illustrator uses points (1 inch = 72 points)
+    var newWidth = border.width - 2 * offset;
+    var newHeight = border.height - 2 * offset;
+    var deltaX = (border.width - newWidth) / 2;
+    var deltaY = (border.height - newHeight) / 2;
+
+    border.resize((newWidth / border.width) * 100, (newHeight / border.height) * 100);
+    border.translate(deltaX, -deltaY);
+    border.zOrder(ZOrderMethod.BRINGTOFRONT);
+})();
